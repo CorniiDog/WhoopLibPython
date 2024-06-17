764e18c44bff104681a249071f4202846398bacb
ConnorAtmos <connor.sw.personal@gmail.com>
2024-06-17 14:58:10 -0500

    Additional initializations

--- main.py
+++ main.py
@@ -26,14 +26,15 @@ def main():
     # Image of the transformation origins: https://files.readme.io/29080d9-Tracking_Depth_fixture_image.png
     d43i_pose = offsetCalculator.OffsetTransform(t265_pose, px=-16/1000, py=36.40/1000, pz=-4/1000) 
 
-    # This is the pose messenger system for the V5 Brain
-    pose_messenger = bufferNode.Messenger(buffer_system, "P")
-    
     # This is the pose of the robot. Same rules apply. It is relative to the pose of the T265 camera
     # The center bottom of the robot is 150 millimeters below camera (since the camera is obviously higher than the bottom of the robot)
     # The camera is perfectly centered on the front of the robot, so px doesn't need to be changed, and neither does the rx, ry, rz, and rw values for rotation.
     # The center of mass of the robot is about 196 millimeters behind the camera, so we set pz to 196/1000 to convert to meters.
     robot_pose = offsetCalculator.OffsetTransform(t265_pose, px=0, py=-150/1000, pz=196/1000)
+
+    # This is the pose messenger system for the V5 Brain. Communication
+    pose_messenger = bufferNode.Messenger(buffer_system, stream="P")
+
 
     # Register the t265_pose to send robot_pose over the pose_messenger stream whenever the t265_pose updates
     # This allows instantenous sending as soon as data is received, therefore reducing delay
